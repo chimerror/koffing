8fa32da549336b78165590194def181097ab8579
Jaycie Mitchell <foobardog@hotmail.com>
2026-08-15 19:46:23 -0700

    TileTests: Converting Indentation to Tabs

--- Tests/TileTests.cs
+++ Tests/TileTests.cs
@@ -5,106 +5,106 @@ using static GdUnit4.Assertions;
 [TestSuite]
 public class TileTests
 {
-    [TestCase]
-    public static void RawRanksAreCorrect()
-    {
-        foreach (var suit in Enum.GetValues<Suit>())
-        {
-            for (var rank = 0; rank <= 9; rank++)
-            {
-                if (!IsValidTile(suit, rank))
-                {
-                    continue;
-                }
+	[TestCase]
+	public static void RawRanksAreCorrect()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (var rank = 0; rank <= 9; rank++)
+			{
+				if (!IsValidTile(suit, rank))
+				{
+					continue;
+				}
 
-                var tile = new Tile(suit, rank);
-                var expectedRawRank = rank == 0 ? 5 : rank;
-                AssertThat(tile.RawRank).IsEqual(expectedRawRank);
-            }
-        }
-    }
+				var tile = new Tile(suit, rank);
+				var expectedRawRank = rank == 0 ? 5 : rank;
+				AssertThat(tile.RawRank).IsEqual(expectedRawRank);
+			}
+		}
+	}
 
-    [TestCase]
-    public static void RawEqualsIsCorrectPositiveCases()
-    {
-        foreach (var suit in Enum.GetValues<Suit>())
-        {
-            for (var rank = 0; rank <= 9; rank++)
-            {
-                if (!IsValidTile(suit, rank))
-                {
-                    continue;
-                }
+	[TestCase]
+	public static void RawEqualsIsCorrectPositiveCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (var rank = 0; rank <= 9; rank++)
+			{
+				if (!IsValidTile(suit, rank))
+				{
+					continue;
+				}
 
-                var tileA = new Tile(suit, rank);
-                var tileB = new Tile(suit, rank);
+				var tileA = new Tile(suit, rank);
+				var tileB = new Tile(suit, rank);
 
-                AssertThat(tileA.RawEquals(tileB)).IsTrue(); // tile A should rawly equal identical tile B
-                AssertThat(tileB.RawEquals(tileA)).IsTrue(); // tile B should rawly equal identical tile A
+				AssertThat(tileA.RawEquals(tileB)).IsTrue(); // tile A should rawly equal identical tile B
+				AssertThat(tileB.RawEquals(tileA)).IsTrue(); // tile B should rawly equal identical tile A
 
-                if (rank == 0)
-                {
-                    var tileC = new Tile(suit, 5);
-                    AssertThat(tileA.RawEquals(tileC)).IsTrue(); // red five A should rawly equal non-red five C
-                    AssertThat(tileC.RawEquals(tileA)).IsTrue(); // non-red five C should rawly equal red five A
-                }
-            }
-        }
-    }
+				if (rank == 0)
+				{
+					var tileC = new Tile(suit, 5);
+					AssertThat(tileA.RawEquals(tileC)).IsTrue(); // red five A should rawly equal non-red five C
+					AssertThat(tileC.RawEquals(tileA)).IsTrue(); // non-red five C should rawly equal red five A
+				}
+			}
+		}
+	}
 
-    [TestCase]
-    public static void RawEqualsIsCorrectNegativeCases()
-    {
-        foreach (var suitA in Enum.GetValues<Suit>())
-        {
-            for (var rankA = 0; rankA <= 9; rankA++)
-            {
-                if (!IsValidTile(suitA, rankA))
-                {
-                    continue;
-                }
+	[TestCase]
+	public static void RawEqualsIsCorrectNegativeCases()
+	{
+		foreach (var suitA in Enum.GetValues<Suit>())
+		{
+			for (var rankA = 0; rankA <= 9; rankA++)
+			{
+				if (!IsValidTile(suitA, rankA))
+				{
+					continue;
+				}
 
-                var tileA = new Tile(suitA, rankA);
-                foreach (var suitB in Enum.GetValues<Suit>())
-                {
-                    if (suitA == suitB || !IsValidTile(suitB, rankA))
-                    {
-                        continue;
-                    }
+				var tileA = new Tile(suitA, rankA);
+				foreach (var suitB in Enum.GetValues<Suit>())
+				{
+					if (suitA == suitB || !IsValidTile(suitB, rankA))
+					{
+						continue;
+					}
 
-                    var tileB = new Tile(suitB, rankA);
+					var tileB = new Tile(suitB, rankA);
 
-                    // tile A should not rawly equal tile B with a different suit but the same rank
-                    AssertThat(tileA.RawEquals(tileB)).IsFalse();
+					// tile A should not rawly equal tile B with a different suit but the same rank
+					AssertThat(tileA.RawEquals(tileB)).IsFalse();
 
-                    // tile B should not rawly equal tile A with a different suit but the same rank
-                    AssertThat(tileB.RawEquals(tileA)).IsFalse();
-                }
+					// tile B should not rawly equal tile A with a different suit but the same rank
+					AssertThat(tileB.RawEquals(tileA)).IsFalse();
+				}
 
-                for (var rankC = 0; rankC <= 9; rankC++)
-                {
-                    if (rankA == rankC ||
-                        (rankA == 5 && rankC == 0) ||
-                        (rankA == 0 && rankC == 5)||
-                        !IsValidTile(suitA, rankC))
-                    {
-                        continue;
-                    }
+				for (var rankC = 0; rankC <= 9; rankC++)
+				{
+					if (rankA == rankC ||
+						(rankA == 5 && rankC == 0) ||
+						(rankA == 0 && rankC == 5)||
+						!IsValidTile(suitA, rankC))
+					{
+						continue;
+					}
 
-                    var tileC = new Tile(suitA, rankC);
+					var tileC = new Tile(suitA, rankC);
 
-                    // tile A should not rawly equal tile C with a different rank but the same suit
-                    AssertThat(tileA.RawEquals(tileC)).IsFalse();
+					// tile A should not rawly equal tile C with a different rank but the same suit
+					AssertThat(tileA.RawEquals(tileC)).IsFalse();
 
-                    // tile C should not rawly equal tile A with a different rank but the same suit
-                    AssertThat(tileC.RawEquals(tileA)).IsFalse();
-                }
-            }
-        }
-    }
+					// tile C should not rawly equal tile A with a different rank but the same suit
+					AssertThat(tileC.RawEquals(tileA)).IsFalse();
+				}
+			}
+		}
+	}
 
-    private static bool IsValidTile(Suit suit, int rank)
-    {
-        return suit != Suit.Zi ? (rank >= 0 && rank <= 9) : (rank >= 1 && rank <= 7);
-    }
+	private static bool IsValidTile(Suit suit, int rank)
+	{
+		return suit != Suit.Zi ? (rank >= 0 && rank <= 9) : (rank >= 1 && rank <= 7);
+	}
 }
