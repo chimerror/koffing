8829645fb1cfb3258323766b9a0a4c20809e936e
Jaycie "chimerror" Mitchell <chimerror@live.com>
2026-09-22 17:40:07 -0700

    Pung: Implementing IDowngradable for Pair and IUpgradable for Kong

--- Scripts/Melds/Pung.cs
+++ Scripts/Melds/Pung.cs
@@ -1,8 +1,37 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Pung : Meld, IBlock
+public class Pung : Meld, IBlock, IDowngradable<Pair>, IUpgradable<Kong>
 {
+	HashSet<Tile> IUpgradable<Kong>.SoughtTiles
+	{
+		get
+		{
+			var suit = _tiles[0].Suit;
+			var rank = _tiles[0].RawRank;
+			if (suit != Suit.Zi && rank == 5)
+			{
+				var redFive = _tiles.SingleOrDefault(t => t.Rank == 0);
+				var nonRedFives = _tiles.Where(t => t.Rank != 0);
+				var soughtTiles = new HashSet<Tile>();
+				if (redFive == default)
+				{
+					soughtTiles.Add(new Tile(suit, 0));
+				}
+				else
+				{
+					soughtTiles.Add(new Tile(suit, 5));
+				}
+				return soughtTiles;
+			}
+			else
+			{
+				return new HashSet<Tile>([_tiles[0]]);
+			}
+		}
+	}
+
 	public Pung(IEnumerable<Tile> tiles = null) : base(tiles)
 	{
 	}
@@ -55,5 +84,44 @@ public class Pung : Meld, IBlock
 	public static new int GetHashCodeBasis()
 	{
 		return 3;
+	}
+
+	IEnumerable<MadeBlockContext> IDowngradable<Pair>.Downgrade()
+	{
+		var redFive = _tiles.SingleOrDefault(t => t.Rank == 0);
+		if (redFive != default)
+		{
+			var nonRedFives = _tiles.Where(t => t.Rank == 5);
+			yield return new MadeBlockContext(
+				new Pair(nonRedFives.Take(1).Append(redFive)),
+				nonRedFives.Skip(1)
+			);
+			yield return new MadeBlockContext(
+				new Pair(nonRedFives),
+				[redFive]
+			);
+		}
+		else
+		{
+			yield return new MadeBlockContext(
+				new Pair(_tiles.Take(2)),
+				_tiles.Skip(2)
+			);
+		}
+	}
+
+	bool IUpgradable<Kong>.CanUpgradeWith(Tile tile)
+	{
+		return tile.Suit == _tiles[0].Suit && tile.RawRank == _tiles[0].RawRank;
+	}
+
+	Kong IUpgradable<Kong>.Upgrade(Tile tile)
+	{
+		if (!((IUpgradable<Kong>)this).CanUpgradeWith(tile))
+		{
+			throw new ArgumentException($"Attempted to upgrade kong of {_tiles[0]} with invalid tile {tile}");
+		}
+
+		return new Kong(_tiles.Append(tile));
 	}
 }
--- Scripts/Tests/UpgradableDowngradableTests.cs
+++ Scripts/Tests/UpgradableDowngradableTests.cs
@@ -64,6 +64,62 @@ public class UpgradableDowngradableTests
 		AssertArray(expectedOutput).ContainsExactlyInAnyOrder(actualOutput);
 	}
 
+	[TestCase]
+	[DataPoint(nameof(DowngradePungToPairTestCases))]
+	public static void DowngradePungToPairIsCorrect(Pung pung, IEnumerable<MadeBlockContext> expectedOutput)
+	{
+		LoggingPrefix = nameof(DowngradePungToPairIsCorrect);
+
+		var actualOutput = ((IDowngradable<Pair>)pung).Downgrade();
+		PrefixInfo($"Checking that downgrading pung \"{pung.Tiles.NotationFromTiles()}\" to pair works");
+		AssertArray(expectedOutput).ContainsExactlyInAnyOrder(actualOutput);
+	}
+
+	[TestCase]
+	[DataPoint(nameof(UpgradePungToKongSoughtTilesTestCases))]
+	public static void UpgradePungToKongSoughtTilesIsCorrect(Pung pung, HashSet<Tile> expectedOutput)
+	{
+		LoggingPrefix = nameof(UpgradePungToKongSoughtTilesIsCorrect);
+
+		var actualOutput = ((IUpgradable<Kong>)pung).SoughtTiles;
+		PrefixInfo($"Checking that tiles sought for upgrading pung \"{pung.Tiles.NotationFromTiles()}\" to kong works");
+		AssertArray(expectedOutput).ContainsExactlyInAnyOrder(actualOutput);
+	}
+
+	[TestCase]
+	[DataPoint(nameof(UpgradePungToKongCanUpgradeWithTestCases))]
+	public static void UpgradePungToKongCanUpgradeWithIsCorrect(Pung pung, Tile tile, bool expectedResult, string because)
+	{
+		LoggingPrefix = nameof(UpgradePungToKongCanUpgradeWithIsCorrect);
+
+		var actualResult = ((IUpgradable<Kong>)pung).CanUpgradeWith(tile);
+		var outcomeString = expectedResult ? "can" : "can NOT";
+		PrefixInfo($"Checking that tile \"{tile}\" {outcomeString} upgrade pung \"{pung.Tiles.NotationFromTiles()}\" to kong when given {because}");
+		AssertThat(expectedResult).IsEqual(actualResult);
+	}
+
+	[TestCase]
+	[ThrowsException(typeof(ArgumentException))]
+	[DataPoint(nameof(UpgradePungToKongNegativeTestCases))]
+	public static void UpgradePungToKongNegativeCasesAreCorrect(Pung pung, Tile tile, string because)
+	{
+		LoggingPrefix = nameof(UpgradePungToKongNegativeCasesAreCorrect);
+
+		PrefixInfo($"Checking that trying upgrade pung \"{pung.Tiles.NotationFromTiles()}\" to kong with tile \"{tile}\" throws an ArgumentException when given {because}");
+		((IUpgradable<Kong>)pung).Upgrade(tile); // will throw
+	}
+
+	[TestCase]
+	[DataPoint(nameof(UpgradePungToKongPositiveTestCases))]
+	public static void UpgradePungToKongPositiveCasesAreCorrect(Pung pung, Tile tile, Kong expectedOutput, string because)
+	{
+		LoggingPrefix = nameof(UpgradePungToKongPositiveCasesAreCorrect);
+
+		var actualOutput = ((IUpgradable<Kong>)pung).Upgrade(tile); // will throw
+		PrefixInfo($"Checking that it is possible to upgrade pung \"{pung.Tiles.NotationFromTiles()}\" to kong with tile \"{tile}\" when given {because}");
+		AssertThat(expectedOutput).IsEqual(actualOutput);
+	}
+
 	private static IEnumerable<object[]> DowngradeChowToKanchanTestCases()
 	{
 		foreach (var suit in Enum.GetValues<Suit>())
@@ -200,6 +256,383 @@ public class UpgradableDowngradableTests
 						{
 							new(new Pung(tiles.Take(3)), tiles.Skip(3)),
 						},
+					];
+				}
+			}
+		}
+	}
+
+	private static IEnumerable<object[]> DowngradePungToPairTestCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (int rank = 1; rank <=9; rank++)
+			{
+				if (suit == Suit.Zi && rank > 7)
+				{
+					break;
+				}
+
+				var suitString = suit.GetSuitString();
+				if (suit != Suit.Zi && rank == 5)
+				{
+					var nonRedFives = $"555{suitString}".ToTiles();
+					var redFive = $"0{suitString}".ToTile();
+					yield return
+					[
+						new Pung(nonRedFives),
+						new List<MadeBlockContext>()
+						{
+							new(new Pair(nonRedFives.Take(2)), nonRedFives.Skip(2)),
+						}
+					];
+					yield return
+					[
+						new Pung(nonRedFives.Take(2).Append(redFive)),
+						new List<MadeBlockContext>()
+						{
+							new(new Pair(nonRedFives.Take(2)), [redFive]),
+							new(new Pair(nonRedFives.Take(1).Append(redFive)), nonRedFives.Skip(1).Take(1)),
+						}
+					];
+				}
+				else
+				{
+					var tiles = $"{rank}{rank}{rank}{suitString}".ToTiles();
+					yield return
+					[
+						new Pung(tiles),
+						new List<MadeBlockContext>()
+						{
+							new(new Pair(tiles.Take(2)), tiles.Skip(2)),
+						},
+					];
+				}
+			}
+		}
+	}
+
+	private static IEnumerable<object[]> UpgradePungToKongSoughtTilesTestCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (int rank = 1; rank <=9; rank++)
+			{
+				if (suit == Suit.Zi && rank > 7)
+				{
+					break;
+				}
+
+				var suitString = suit.GetSuitString();
+				if (suit != Suit.Zi && rank == 5)
+				{
+					var nonRedFives = $"555{suitString}".ToTiles();
+					var redFive = $"0{suitString}".ToTile();
+					yield return
+					[
+						new Pung(nonRedFives),
+						new HashSet<Tile>([redFive]),
+					];
+					yield return
+					[
+						new Pung(nonRedFives.Take(2).Append(redFive)),
+						new HashSet<Tile>(nonRedFives.Skip(2)),
+					];
+				}
+				else
+				{
+					var tiles = $"{rank}{rank}{rank}{suitString}".ToTiles();
+					yield return
+					[
+						new Pung(tiles),
+						new HashSet<Tile>(tiles.Take(1)),
+					];
+				}
+			}
+		}
+	}
+
+	private static IEnumerable<object[]> UpgradePungToKongCanUpgradeWithTestCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (int rank = 1; rank <=9; rank++)
+			{
+				if (suit == Suit.Zi && rank > 7)
+				{
+					break;
+				}
+
+				var suitString = suit.GetSuitString();
+				var tiles = $"{rank}{rank}{rank}{suitString}".ToTiles();
+				var redFivesPossible = suit != Suit.Zi && rank == 5;
+				yield return
+				[
+					new Pung(tiles),
+					tiles.First(),
+					true,
+					$"a matching tile",
+				];
+
+				if (redFivesPossible)
+				{
+					var redFive = new Tile(suit, 0);
+					yield return
+					[
+						new Pung(tiles),
+						redFive,
+						true,
+						$"a matching red five",
+					];
+					yield return
+					[
+						new Pung(tiles.Take(2).Append(redFive)),
+						new Tile(suit, 5),
+						true,
+						$"a matching non-red five",
+					];
+				}
+
+				foreach (var differentSuit in Enum.GetValues<Suit>())
+				{
+					if (suit == differentSuit || !Tile.IsValidTile(differentSuit, rank))
+					{
+						continue;
+					}
+
+					var differentSuitTile = new Tile(differentSuit, rank);
+					yield return
+					[
+						new Pung(tiles),
+						differentSuitTile,
+						false,
+						$"a tile of same rank but different suit",
+					];
+
+					if (redFivesPossible)
+					{
+						var redFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles.Take(2).Append(redFive)),
+							differentSuitTile,
+							false,
+							$"a tile of same rank but different suit with a red five in the pung",
+						];
+					}
+
+					var differentSuitRedFivesPossible = suit != Suit.Zi;
+					if (differentSuitRedFivesPossible)
+					{
+						var differentSuitRedFive = new Tile(differentSuit, 0);
+						yield return
+						[
+							new Pung(tiles),
+							differentSuitRedFive,
+							false,
+							$"a red five of a different suit",
+						];
+
+						if (redFivesPossible)
+						{
+							var redFive = new Tile(suit, 0);
+							yield return
+							[
+								new Pung(tiles.Take(2).Append(redFive)),
+								differentSuitRedFive,
+								false,
+								$"a red five of a different suit with a red five in the pung",
+							];
+						}
+					}
+				}
+
+				for (int differentRank = 1; differentRank <= 9; differentRank++)
+				{
+					if (rank == differentRank || !Tile.IsValidTile(suit, differentRank))
+					{
+						continue;
+					}
+
+					var differentRankTile = new Tile(suit, differentRank);
+					yield return
+					[
+						new Pung(tiles),
+						differentRankTile,
+						false,
+						$"a tile of same suit but different rank",
+					];
+
+					if (redFivesPossible)
+					{
+						var redFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles.Take(2).Append(redFive)),
+							differentRankTile,
+							false,
+							$"a tile of same suit but different rank with a red five in the pung",
+						];
+					}
+					else if (suit != Suit.Zi && differentRank == 5)
+					{
+						var differentRankRedFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles),
+							differentRankRedFive,
+							false,
+							$"a red five of same suit but different rank",
+						];
+					}
+				}
+			}
+		}
+	}
+
+	private static IEnumerable<object[]> UpgradePungToKongNegativeTestCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (int rank = 1; rank <=9; rank++)
+			{
+				if (suit == Suit.Zi && rank > 7)
+				{
+					break;
+				}
+
+				var suitString = suit.GetSuitString();
+				var tiles = $"{rank}{rank}{rank}{suitString}".ToTiles();
+				var redFivesPossible = suit != Suit.Zi && rank == 5;
+
+				foreach (var differentSuit in Enum.GetValues<Suit>())
+				{
+					if (suit == differentSuit || !Tile.IsValidTile(differentSuit, rank))
+					{
+						continue;
+					}
+
+					var differentSuitTile = new Tile(differentSuit, rank);
+					yield return
+					[
+						new Pung(tiles),
+						differentSuitTile,
+						$"a tile of same rank but different suit",
+					];
+
+					if (redFivesPossible)
+					{
+						var redFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles.Take(2).Append(redFive)),
+							differentSuitTile,
+							$"a tile of same rank but different suit with a red five in the pung",
+						];
+					}
+
+					var differentSuitRedFivesPossible = suit != Suit.Zi;
+					if (differentSuitRedFivesPossible)
+					{
+						var differentSuitRedFive = new Tile(differentSuit, 0);
+						yield return
+						[
+							new Pung(tiles),
+							differentSuitRedFive,
+							$"a red five of a different suit",
+						];
+
+						if (redFivesPossible)
+						{
+							var redFive = new Tile(suit, 0);
+							yield return
+							[
+								new Pung(tiles.Take(2).Append(redFive)),
+								differentSuitRedFive,
+								$"a red five of a different suit with a red five in the pung",
+							];
+						}
+					}
+				}
+
+				for (int differentRank = 1; differentRank <= 9; differentRank++)
+				{
+					if (rank == differentRank || !Tile.IsValidTile(suit, differentRank))
+					{
+						continue;
+					}
+
+					var differentRankTile = new Tile(suit, differentRank);
+					yield return
+					[
+						new Pung(tiles),
+						differentRankTile,
+						$"a tile of same suit but different rank",
+					];
+
+					if (redFivesPossible)
+					{
+						var redFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles.Take(2).Append(redFive)),
+							differentRankTile,
+							$"a tile of same suit but different rank with a red five in the pung",
+						];
+					}
+					else if (suit != Suit.Zi && differentRank == 5)
+					{
+						var differentRankRedFive = new Tile(suit, 0);
+						yield return
+						[
+							new Pung(tiles),
+							differentRankRedFive,
+							$"a red five of same suit but different rank",
+						];
+					}
+				}
+			}
+		}
+	}
+
+	private static IEnumerable<object[]> UpgradePungToKongPositiveTestCases()
+	{
+		foreach (var suit in Enum.GetValues<Suit>())
+		{
+			for (int rank = 1; rank <=9; rank++)
+			{
+				if (suit == Suit.Zi && rank > 7)
+				{
+					break;
+				}
+
+				var suitString = suit.GetSuitString();
+				var tiles = $"{rank}{rank}{rank}{suitString}".ToTiles();
+				var redFivesPossible = suit != Suit.Zi && rank == 5;
+				yield return
+				[
+					new Pung(tiles),
+					tiles.First(),
+					new Kong(tiles.Append(new Tile(suit, rank))),
+					$"a matching tile",
+				];
+
+				if (redFivesPossible)
+				{
+					var redFive = new Tile(suit, 0);
+					yield return
+					[
+						new Pung(tiles),
+						redFive,
+						new Kong(tiles.Append(redFive)),
+						$"a matching red five",
+					];
+					yield return
+					[
+						new Pung(tiles.Take(2).Append(redFive)),
+						new Tile(suit, 5),
+						new Kong(tiles.Append(redFive)), // a bit wonky, but equivalent
+						$"a matching non-red five",
 					];
 				}
 			}
