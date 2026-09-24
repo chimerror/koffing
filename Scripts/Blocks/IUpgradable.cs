2074d97f6cb5419280b4a9058e5bb485e479cbc8
Jaycie "chimerror" Mitchell <chimerror@live.com>
2026-09-23 23:02:46 -0700

    TODO on determining necessity of IUpgradable/IDowngradable

--- Scripts/Blocks/IUpgradable.cs
+++ Scripts/Blocks/IUpgradable.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 
+// TODO: I am questioning if this interface and its cousin IDowngradable is worth doing. I am going to pivot over to
+// doing match setup, which I am hoping will make that question as far as others clearer. Based on that, either remove
+// these interfaces, or finish implementing them for Pair and the Waits.
 public interface IUpgradable<T>
 {
 	HashSet<Tile> SoughtTiles { get; }
